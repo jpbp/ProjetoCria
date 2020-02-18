325ed9d0f5da8387c4fca2c5da4e2d2e01519bd6
jpbp <jpbp@estudante.ufla.br>
2020-02-18 13:46:52 -0300

    Classe Carro comentada

--- ProvaCria/src/provacria/Carro.java
+++ ProvaCria/src/provacria/Carro.java
@@ -14,12 +14,12 @@ import java.io.Serializable;
 public class Carro implements Serializable {
     String nome;
     private static final long serialVersionUID =  -4419856798427564309L;
-
+    //Connstrutor 
     public Carro(String nome) {
         this.nome = nome;
         
     }
-
+    //metodo para obter o nome
     public String getNome() {
         return nome;
     }
