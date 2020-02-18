22fc4772de55eecee6cfb989b774182729f67b10
jpbp <jpbp@estudante.ufla.br>
2020-02-18 13:47:33 -0300

    Classe Taxa comentada

--- ProvaCria/src/provacria/Taxa.java
+++ ProvaCria/src/provacria/Taxa.java
@@ -15,16 +15,16 @@ public class Taxa implements Serializable {
     double semana;
     double finalDeSemana;
     private static final long serialVersionUID =   6792098431215253667L;
-
+    //Construtor 
     public Taxa(double semana, double finalDeSemana) {
         this.semana = semana;
         this.finalDeSemana = finalDeSemana;
     }
-
+    //metodo para obter o a taxa da semana
     public double getSemana() {
         return semana;
     }
-
+    //metodo para obter o a taxa do fim de semana
     public double getFinalDeSemana() {
         return finalDeSemana;
     }
